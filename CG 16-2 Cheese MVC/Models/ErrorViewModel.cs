baaad5ca2ef8f512c4ef0d00d570d26de1787278
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-11-22 22:16:54 -0600

    Add project files.

---
+++ CG 16-2 Cheese MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CG_16_2_Cheese_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
